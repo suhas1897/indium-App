4c834ac7912c6f99d260dd623a3f1ca44af8d72b
suhas1897 <chalamcharlasuhas911@gmail.com>
2024-05-27 11:08:34 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import fonts from "./config/fonts";
+
+import Navigation from "./navigation";
+import React from "react";
+
+export default function App() {
+  const [fontsLoaded] = useFonts(fonts);
+
+  return !fontsLoaded ? null : (
+    <SafeAreaProvider>
+      <Navigation />
+      <StatusBar />
+    </SafeAreaProvider>
+  );
+}
